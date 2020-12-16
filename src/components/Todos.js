afe74f26a36e07103bcb1d2ee9c92dd2bfbbab5c
enginecode-cpu <bassmatics1002@gmail.com>
2020-12-16 20:26:01 +0900

    Complete!

---
+++ src/components/Todos.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+function TodoItems({ todo, onToggle, onRemove }) {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        onClick={() => onToggle(todo.id)}
+        checked={todo.done}
+        readOnly={true}
+      />
+      <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+      <button onClick={() => onRemove(todo.id)}>삭제</button>
+    </div>
+  );
+}
+
+function Todos({ input, todos, onChangeInput, onInsert, onToggle, onRemove }) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    onInsert(input);
+    onChangeInput('');
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    onChangeInput(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={input} onChange={onChange} />
+        <button type="submit">등록</button>
+        <div>
+          {todos.map((todo) => (
+            <TodoItems
+              todo={todo}
+              key={todo.id}
+              onToggle={onToggle}
+              onRemove={onRemove}
+            />
+          ))}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Todos;
